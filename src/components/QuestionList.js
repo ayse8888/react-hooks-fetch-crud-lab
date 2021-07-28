52bc525ac63debaf525509a14c808d7e2132cfa3
ayse8888 <aysebasar@yahoo.com>
2021-07-28 22:23:41 +0300

    commit tree 4229328d85f6676eb21d753ca57f0fc94ecc7b50

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,38 @@
+
 import React from "react";
+import { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((response ) => response.json())
+      .then((data) => {
+        console.log(data)
+        setQuestions(data);
+      });
+  }, []);
+
+
+  function deleteQuestions(id){
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: 'DELETE',
+      headers: { 'Content-type': 'application/json' }
+    })
+    .then(response => response.json())
+    .then(() => setQuestions(questions.filter(item => item.id !== id)));
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questions.map(question => 
+        <QuestionItem key={question.id} question={question} deleteQuestions={deleteQuestions} />
+       )}
+      </ul>
     </section>
   );
 }
